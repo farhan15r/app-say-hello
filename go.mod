f0668e6574c8e50700f296b9c7685b675c716642
farhan ramadhan <nak.rokesselalu@gmail.com>
2023-08-08 19:52:05 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/farhan15r/app-say-hello.git
+
+go 1.20
+
+require github.com/farhan15r/go-say-hello/v2 v2.0.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	gosayhello "github.com/farhan15r/go-say-hello/v2"
+)
+
+func main() {
+	fmt.Println(gosayhello.SayHello("Farhan"))
+}
